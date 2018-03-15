17a57032a1e188b44300baa1ce2fcb96d2af3f44
Kaipheu <37062977+Kaipheu@users.noreply.github.com>
2018-03-15 14:53:44 +0100

    Mesure pour photoresistance

---
+++ Mesure/Mesure.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4)
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(13,INPUT_PULLUP);
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+}
+
+void loop() {
+  if(digitalRead(13)){
+      lcd.println(analogRead(13));
+    }
+}

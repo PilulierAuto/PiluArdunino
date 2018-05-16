c7d14ab431786db2c592a1da57c0fb2bbc73f3f7
Fractal07 <baptiste.merlau@vivaldi.net>
2018-05-16 16:54:27 +0200

    Modif

---
+++ code/Actu.ino
@@ -0,0 +1,64 @@
+
+
+
+void ActuPos(){
+  String NomTemp; //INIT
+  String MedStr;
+  char charTemp[16];
+  byte f=0, e=0, d=25, i=0;
+  EEPROM.get(0, Med);
+  
+  while (!Serial.available()){} //On attend que l'utilisateur entre son nom
+  if(Serial.available()){
+    NomTemp=Serial.readString();
+  }
+  NomTemp.toCharArray(charTemp, 16);
+  
+  for(e=0;e<5&&d==25;e++){          //On le compare à ceux déjà existants
+    if(strcmp(charTemp, Med[e].Nom)) //et on détermine qui c'est (variable d)
+      d=e;
+  }
+
+  if(d==25){                        //Le nom n'existe pas : envoi du signal d'erreur et fin
+    Serial.write(ActuPos_Id_err);
+  }
+  else{                            //Le nom existe : envoi du signal OK
+    Serial.write(ActuPos_Id_OK);
+    while (!Serial.available()){}  //On attend  que l'utilisateur entre sa posologie
+    while (Serial.available()){
+      if (Serial.read() == ActuPos_Debut){  //Signal de début de la réception
+        MedStr=Serial.readString();
+
+        for(i=0;i<=4;i++){        //On copie les valeurs dans la structure provisoire
+          Med[d].mat[i]=MedStr.charAt(i)-48;
+          }         
+        for(i=5;i<=9;i++){        //On copie les valeurs dans la structure provisoire
+          Med[d].midi[i-5]=MedStr.charAt(i)-48;
+          }
+        for(i=10;i<=14;i++){        //On copie les valeurs dans la structure provisoire
+          Med[d].soir[i-10]=MedStr.charAt(i)-48;
+          }
+        for(i=15;i<=19;i++){        //On copie les valeurs dans la structure provisoire
+          Med[d].nuit[i-15]=MedStr.charAt(i)-48;
+          } 
+        for(i=20;i<=24;i++){        //On copie les valeurs dans la structure provisoire
+          Med[d].midi[i-20]=MedStr.charAt(i)-48;
+          } 
+        EEPROM.put(0, Med);     //On rentre dans la mémoire EEProm
+        }
+    }
+  }    
+}
+
+void ActuRTC(){
+  int Hr_temp=0, Mn_temp=0;
+  while(Hr_temp<=0)
+    Hr_temp=Serial.parseInt();
+  while(Mn_temp<=0)
+    Mn_temp=Serial.parseInt();
+  RTC.read(tm);
+  if(tm.Hour!=Hr_temp || tm.Minute!=Mn_temp){
+    setTime(Hr_temp, Mn_temp, tm.Second, tm.Day, tm.Month, tm.Year);
+    RTC.set(now());
+  }
+}
---
+++ code/Moteur.ino
@@ -0,0 +1,177 @@
+void RotPlat(){
+  boolean fin=Fin();
+  int temp = 0;
+  if((MedSeq[(PosPlat+1)%5]!=0 && PosPlat+1<=4) || fin&&PosPlat==4){
+    analogWrite(M1Pwm, 45);
+    digitalWrite(M1HorPin, HIGH);
+    digitalWrite(M1TrigPin, LOW);
+    delay(100);
+  
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+  
+    digitalWrite(M1HorPin, HIGH); //arrêt moteur (frein)
+    delay(100);
+
+    analogWrite(M1Pwm, 0);       //arrêt moteur  
+    digitalWrite(M1HorPin, LOW);
+    digitalWrite(M1TrigPin, LOW);
+    if(PosPlat!=4)
+      PosPlat++;
+    else
+      PosPlat=0;
+  }
+  else if(MedSeq[(PosPlat+2)%5]!=0 && PosPlat+2<=4 || fin&&PosPlat==3){
+    analogWrite(M1Pwm, 100);
+    digitalWrite(M1HorPin, HIGH);
+    digitalWrite(M1TrigPin, LOW);
+    delay(100);
+  
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    delay(200);
+    analogWrite(M1Pwm, 45);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    
+  
+    digitalWrite(M1HorPin, HIGH); //arrêt moteur (frein)
+    delay(100);
+
+    analogWrite(M1Pwm, 0);       //arrêt moteur  
+    digitalWrite(M1HorPin, LOW);
+    digitalWrite(M1TrigPin, LOW);
+    PosPlat+=2;
+  }
+    else if(MedSeq[(PosPlat+3)%5]!=0 && PosPlat+3<=4 || fin&&PosPlat==2){
+    analogWrite(M1Pwm, 100);
+    digitalWrite(M1HorPin, HIGH);
+    digitalWrite(M1TrigPin, LOW);
+    delay(100);
+  
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    delay(200);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    analogWrite(M1Pwm, 45);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    
+    digitalWrite(M1HorPin, HIGH); //arrêt moteur (frein)
+    delay(100);
+
+    analogWrite(M1Pwm, 0);       //arrêt moteur  
+    digitalWrite(M1HorPin, LOW);
+    digitalWrite(M1TrigPin, LOW);
+    PosPlat+=3;
+  }
+  else if(MedSeq[(PosPlat+4)%5]!=0 && PosPlat+4<=4 || fin&&PosPlat==1){
+    analogWrite(M1Pwm, 45);
+      digitalWrite(M1HorPin, HIGH);
+    digitalWrite(M1TrigPin, LOW);
+    delay(100);
+  
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    delay(200);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    delay(200);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    delay(200);
+    analogWrite(M1Pwm, 100);
+    do{
+      temp = digitalRead(CptCtcPin); 
+    }while(temp==0);
+    
+    digitalWrite(M1HorPin, HIGH); //arrêt moteur (frein)
+    delay(100);
+
+    analogWrite(M1Pwm, 0);       //arrêt moteur  
+    digitalWrite(M1HorPin, LOW);
+    digitalWrite(M1TrigPin, LOW);
+    PosPlat+=4;
+  } 
+  PosPlat=PosPlat%5;
+}
+
+void AddMed(int Nbr){
+  int i = 0, j = 0, ref = 0;
+
+  analogWrite(M2Pwm, 255); 
+  digitalWrite(M2HorPin, LOW);
+  digitalWrite(M2TrigPin, HIGH);
+  digitalWrite(IREmmPin, HIGH);
+  ref = analogRead(IRRecPin) -50; 
+
+  while(i<Nbr){
+    do{
+      j = analogRead(IRRecPin);
+    }while(j>ref);
+    Nbr++;
+    delay(100);
+  }
+  digitalWrite(M2HorPin, HIGH); //arrêt moteur (frein)
+  delay(100);
+
+  digitalWrite(M2TrigPin, LOW);
+  delay(1000);
+
+  digitalWrite(M2TrigPin, HIGH);
+  delay(1000);
+  
+  analogWrite(M2Pwm, 0);       //arrêt moteur  
+  digitalWrite(M2HorPin, LOW);
+  digitalWrite(M2TrigPin, LOW);
+  MedSeq[PosPlat]=0;
+}
+
+void RempTab(){
+  RTC.read(tm);
+  int i=0;
+  if(tm.Hour<10){
+    for(i=0;i<=4;i++){
+      MedSeq[i]=Med[Id].mat[i];
+    }
+  }
+  else if(tm.Hour<15){
+    for(i=0;i<=4;i++){
+      MedSeq[i]=Med[Id].midi[i];
+    }
+  }
+  else if(tm.Hour<21){
+   for(i=0;i<=4;i++){
+      MedSeq[i]=Med[Id].soir[i];
+    } 
+  }
+  else{
+   for(i=0;i<=4;i++){
+      MedSeq[i]=Med[Id].nuit[i]; 
+   }
+  } 
+}
+
+
+
+
+boolean Fin(){
+  int i=0;
+    for(i=0; i<5; i++)
+    {
+        if(MedSeq[i]!=0)
+        return 0;
+    }
+    return 1; 
+}
+
